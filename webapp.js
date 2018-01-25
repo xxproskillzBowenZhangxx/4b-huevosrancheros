ee56f1ae8ba7b4289a889b1481d74d6ed3129c88
Joseph Kaestner <joseph.kaestner@warriorlife.net>
2018-01-25 14:36:49 -0800

    made dummy tags so we can just insert the variables

--- webapp.js
+++ webapp.js
@@ -1,1 +1,33 @@
 // JavaScript Document
+function achoiceFunction(){
+	if(atext == true)  {
+		alert("Correct");
+		}
+	else{
+		alert("Incorrect");
+	}
+}
+function bchoiceFunction(){
+	if(btext == true)  {
+		alert("Correct");
+		}
+	else{
+		alert("Incorrect");
+	}
+}
+function cchoiceFunction(){
+	if(ctext == true)  {
+		alert("Correct");
+		}
+	else{
+		alert("Incorrect");
+	}
+}
+function dchoiceFunction(){
+	if(dtext == true)  {
+		alert("Correct");
+		}
+	else{
+		alert("Incorrect");
+	}
+}
