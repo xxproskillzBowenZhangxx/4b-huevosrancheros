b29ec367f17f49cbce8c95ec23c1f560c712d706
Joseph Kaestner <joseph.kaestner@warriorlife.net>
2018-01-29 14:53:15 -0800

    deleted js
    deleted js

--- webapp.js
+++ webapp.js
@@ -1,33 +1,13 @@
 // JavaScript Document
-function achoiceFunction(){
-	if(atext == true)  {
-		alert("Correct");
-		}
-	else{
-		alert("Incorrect");
+function falsechoiceFunction(){
+alert("Incorrect");
 	}
-}
-function bchoiceFunction(){
-	if(btext == true)  {
-		alert("Correct");
+
+
+function truechoiceFunction(){
+	alert("Correct");
 		}
-	else{
-		alert("Incorrect");
-	}
-}
-function cchoiceFunction(){
-	if(ctext == true)  {
-		alert("Correct");
-		}
-	else{
-		alert("Incorrect");
-	}
-}
-function dchoiceFunction(){
-	if(dtext == true)  {
-		alert("Correct");
-		}
-	else{
-		alert("Incorrect");
-	}
-}
+		var backgroundandquestions
+
+
+
