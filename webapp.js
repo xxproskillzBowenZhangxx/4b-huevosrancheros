37f643162e1938637f6d34d96c8d548699336d2f
Joseph Kaestner <joseph.kaestner@warriorlife.net>
2018-02-02 13:41:51 -0800

    made cases for questions

--- webapp.js
+++ webapp.js
@@ -1,61 +1,131 @@
 // JavaScript Document
-function DoUKnowTheWae(){
-// img 1
-document.body.style.backgroundImage = "url('images/douknowtheway.jpg')";
-// answers
-document.getElementById("a").innerHTML = "Do u know da wae?";
-
-document.getElementById("b").innerHTML = "Tide pods r delicious";
-
-document.getElementById("c").innerHTML = "Condesending Willy Wonka";
-
-document.getElementById("d").innerHTML = "Pepe the Frog";
+function changeMeme(){
+	document.getElementById("a").removeEventListener("click", rightAnswer);
+	document.getElementById("a").removeEventListener("click", wrongAnswer);
+	document.getElementById("b").removeEventListener("click", rightAnswer);
+	document.getElementById("b").removeEventListener("click", wrongAnswer);
+	document.getElementById("c").removeEventListener("click", rightAnswer);
+	document.getElementById("c").removeEventListener("click", wrongAnswer);
+	document.getElementById("d").removeEventListener("click", rightAnswer);
+	document.getElementById("d").removeEventListener("click", wrongAnswer);
+	
+	
+	var rand = Math.ceil(Math.random * 50);
+	switch(rand) {
+		case 1: // do you know da wae
+			document.body.style.backgroundImage = "url('images/douknowtheway.jpg')";
+			document.getElementById("a").innerHTML = "Do u know da wae?";
+			document.getElementById("a").addEventListener("click", rightAnswer);
+			document.getElementById("b").innerHTML = "Tide pods r delicious";
+			document.getElementById("b").addEventListener("click", wrongAnswer);
+			document.getElementById("c").innerHTML = "Condesending Willy Wonka";
+			document.getElementById("c").addEventListener("click", wrongAnswer);
+			document.getElementById("d").innerHTML = "Pepe the Frog";
+			document.getElementById("d").addEventListener("click", wrongAnswer);
+			break;
+		case 2:
+			document.body.style.backgroundImage = "url('vader+elfonshelf.jpg')";
+			document.getElementById("a").innerHTML = "vader and the elf on the shelf";
+			document.getElementById("a").addEventListener("click", rightAnswer);
+			document.getElementById("b").addEventListener("click", wrongAnswer);
+			document.getElementById("b").innerHTML = "vader and buddy";
+			document.getElementById("b").addEventListener("click", wrongAnswer);
+			document.getElementById("c").innerHTML = "Condesending Willy Wonka";
+			document.getElementById("c").addEventListener("click", wrongAnswer);
+			document.getElementById("d").innerHTML = "the evil of christmas";
+			document.getElementById("d").addEventListener("click", wrongAnswer);
+			break;
+		case 3:
+			document.body.style.backgroundImage = "url('images/yourenext.jpg')";
+			document.getElementById("a").innerHTML = "the poker";
+			document.getElementById("a").addEventListener("click", wrongAnswer);
+			document.getElementById("b").innerHTML = "you're next";
+			document.getElementById("b").addEventListener("click", rightAnswer);
+			document.getElementById("c").innerHTML = "bowl cut billy";
+			document.getElementById("c").addEventListener("click", wrongAnswer);
+			document.getElementById("d").innerHTML = "Pepe the Frog";
+			document.getElementById("d").addEventListener("click", wrongAnswer);
+			break;
+		case 4:
+			document.body.style.backgroundImage = "url('images/youdontsay.jpg')";
+			document.getElementById("a").innerHTML = "Condescending Willy Wonka";
+			document.getElementById("a").addEventListener("click", wrongAnswer);
+			document.getElementById("b").innerHTML = "trump is going to run for president?";
+			document.getElementById("b").addEventListener("click", wrongAnswer);
+			document.getElementById("c").innerHTML = "bowl cut billy";
+			document.getElementById("c").addEventListener("click", wrongAnswer);
+			document.getElementById("d").innerHTML = "you don't say";
+			document.getElementById("d").addEventListener("click", rightAnswer);
+			break;
+		case 5:
+			document.body.style.backgroundImage = "url('images/yo_dawg.jpg')";
+			document.getElementById("a").innerHTML = "Condescending Willy Wonka";
+			document.getElementById("a").addEventListener("click", wrongAnswer);
+			document.getElementById("b").innerHTML = "trump is going to run for president?";
+			document.getElementById("b").addEventListener("click", wrongAnswer);
+			document.getElementById("c").innerHTML = "yo dawg";
+			document.getElementById("c").addEventListener("click", rightAnswer);
+			document.getElementById("d").innerHTML = "i am your father";
+			document.getElementById("d").addEventListener("click", wrongAnswer);
+			break;
+		case 6: // do you know da wae
+			document.body.style.backgroundImage = "url('images/douknowtheway.jpg')";
+			document.getElementById("a").innerHTML = "Do u know da wae?";
+			document.getElementById("a").addEventListener("click", rightAnswer);
+			document.getElementById("b").innerHTML = "Tide pods r delicious";
+			document.getElementById("b").addEventListener("click", wrongAnswer);
+			document.getElementById("c").innerHTML = "Condesending Willy Wonka";
+			document.getElementById("c").addEventListener("click", wrongAnswer);
+			document.getElementById("d").innerHTML = "Pepe the Frog";
+			document.getElementById("d").addEventListener("click", wrongAnswer);
+			break;
+		case 7:
+			document.body.style.backgroundImage = "url('vader+elfonshelf.jpg')";
+			document.getElementById("a").innerHTML = "vader and the elf on the shelf";
+			document.getElementById("a").addEventListener("click", rightAnswer);
+			document.getElementById("b").addEventListener("click", wrongAnswer);
+			document.getElementById("b").innerHTML = "vader and buddy";
+			document.getElementById("b").addEventListener("click", wrongAnswer);
+			document.getElementById("c").innerHTML = "Condesending Willy Wonka";
+			document.getElementById("c").addEventListener("click", wrongAnswer);
+			document.getElementById("d").innerHTML = "the evil of christmas";
+			document.getElementById("d").addEventListener("click", wrongAnswer);
+			break;
+		case 8:
+			document.body.style.backgroundImage = "url('images/yourenext.jpg')";
+			document.getElementById("a").innerHTML = "the poker";
+			document.getElementById("a").addEventListener("click", wrongAnswer);
+			document.getElementById("b").innerHTML = "you're next";
+			document.getElementById("b").addEventListener("click", rightAnswer);
+			document.getElementById("c").innerHTML = "bowl cut billy";
+			document.getElementById("c").addEventListener("click", wrongAnswer);
+			document.getElementById("d").innerHTML = "Pepe the Frog";
+			document.getElementById("d").addEventListener("click", wrongAnswer);
+			break;
+		case 9:
+			document.body.style.backgroundImage = "url('images/youdontsay.jpg')";
+			document.getElementById("a").innerHTML = "Condescending Willy Wonka";
+			document.getElementById("a").addEventListener("click", wrongAnswer);
+			document.getElementById("b").innerHTML = "trump is going to run for president?";
+			document.getElementById("b").addEventListener("click", wrongAnswer);
+			document.getElementById("c").innerHTML = "bowl cut billy";
+			document.getElementById("c").addEventListener("click", wrongAnswer);
+			document.getElementById("d").innerHTML = "you don't say";
+			document.getElementById("d").addEventListener("click", rightAnswer);
+			break;
+		case 10:
+			document.body.style.backgroundImage = "url('images/yo_dawg.jpg')";
+			document.getElementById("a").innerHTML = "Condescending Willy Wonka";
+			document.getElementById("a").addEventListener("click", wrongAnswer);
+			document.getElementById("b").innerHTML = "trump is going to run for president?";
+			document.getElementById("b").addEventListener("click", wrongAnswer);
+			document.getElementById("c").innerHTML = "yo dawg";
+			document.getElementById("c").addEventListener("click", rightAnswer);
+			document.getElementById("d").innerHTML = "i am your father";
+			document.getElementById("d").addEventListener("click", wrongAnswer);
+			break;
+			
+	}
 }
-function Vader+elfonshelf(){
-// img 1
-document.body.style.backgroundImage = "url('vader+elfonshelf.jpg')";
-// answers
-document.getElementById("a").innerHTML = "vader and the elf on the shelf";
 
-document.getElementById("b").innerHTML = "vader and buddy";
 
-document.getElementById("c").innerHTML = "Condesending Willy Wonka";
-
-document.getElementById("d").innerHTML = "the evil of christmas";
-}
-function DoUKnowTheWae(){
-// img 1
-document.body.style.backgroundImage = "url('images/douknowtheway.jpg')";
-// answers
-document.getElementById("a").innerHTML = "Do u know da wae?";
-
-document.getElementById("b").innerHTML = "Tide pods r delicious";
-
-document.getElementById("c").innerHTML = "Condesending Willy Wonka";
-
-document.getElementById("d").innerHTML = "Pepe the Frog";
-}
-function DoUKnowTheWae(){
-// img 1
-document.body.style.backgroundImage = "url('images/douknowtheway.jpg')";
-// answers
-document.getElementById("a").innerHTML = "Do u know da wae?";
-
-document.getElementById("b").innerHTML = "Tide pods r delicious";
-
-document.getElementById("c").innerHTML = "Condesending Willy Wonka";
-
-document.getElementById("d").innerHTML = "Pepe the Frog";
-}
-function DoUKnowTheWae(){
-// img 1
-document.body.style.backgroundImage = "url('images/douknowtheway.jpg')";
-// answers
-document.getElementById("a").innerHTML = "Do u know da wae?";
-
-document.getElementById("b").innerHTML = "Tide pods r delicious";
-
-document.getElementById("c").innerHTML = "Condesending Willy Wonka";
-
-document.getElementById("d").innerHTML = "Pepe the Frog";
-}
